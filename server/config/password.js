401d6c872c43e089c69b94f317af155e8d491363
DanielMalede <danielmalede@gmail.com>
2022-11-27 14:39:15 +0200

    delete client

--- server/config/password.js
+++ server/config/password.js
@@ -2,15 +2,10 @@ const keys = require("./keys");
 const userModel = require("../model/users-model");
 const jwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
-
+const options = {
+  secretOrKey: keys.secretKey,
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+};
 module.exports = (passport) => {
-  passport.use(
-    new jwtStrategy(
-      {
-        secretOrKey: keys.secretKey,
-        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
-      },
-      () => {}
-    )
-  );
+  passport.use(new jwtStrategy(options, () => {}));
 };
